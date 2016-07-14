821c1c41b1b08790ca2d9f51d7f22c1868ba7be8
karachevtsev <karachevtsev@yahoo.com>
2016-07-15 01:23:05 +0300

    Fixed Homework Task 1.1

--- Lesson 1. Types and Variables/Task-1.1/Task-1.1/Program.cs
+++ Lesson 1. Types and Variables/Task-1.1/Task-1.1/Program.cs
@@ -11,88 +11,79 @@ namespace Task_1._1
         static void Main(string[] args)
         {
             #region Целые типы
+
             // 8-bits == 1 byte
             // Целое без знака. Диапазон - от 0 до 255
-            byte a = 0;
-            Byte b = 255;
-            Console.WriteLine("Type byte:   default value {0}, min {1}, max {2}", 0, a, b);
+            byte myByte = new byte();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myByte.GetType(), myByte, byte.MinValue, byte.MaxValue);
 
             // Целое со знаком. Диапазон - от -128 до +127
-            sbyte c = -128;
-            SByte d = +127;
-            Console.WriteLine("Type sbyte:  default value {0}, min {1}, max {2}", 0, c, d);
+            sbyte mySbyte = new sbyte();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", mySbyte.GetType(), mySbyte, sbyte.MinValue, sbyte.MaxValue);
 
             // 16-bits == 2 bytes
             // Целое без знака. Диапазон - от 0 до 65535
-            ushort e = 0;
-            UInt16 f = 65535;
-            Console.WriteLine("Type ushort: default value {0}, min {1}, max {2}", 0, e, f);
+            ushort myUshort = new ushort();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myUshort.GetType(), myUshort, ushort.MinValue, ushort.MaxValue);
 
             // Целое со знаком. Диапазон - от -32768 до +32767
-            short g = -32768;
-            Int16 h = +32767;
-            Console.WriteLine("Type short:  default value {0}, min {1}, max {2}", 0, g, h);
+            ushort myShort = new ushort();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myShort.GetType(), myShort, short.MinValue, short.MaxValue);
 
             // 32-bits == 4 bytes
             // Целое без знака. Диапазон - от 0 до 4294967295
-            uint i = 0;
-            UInt32 j = 4294967295;
-            Console.WriteLine("Type uint:   default value {0}, min {1}, max {2}", 0, i, j);
+            uint myUint = new uint();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myUint.GetType(), myUint, uint.MinValue, uint.MaxValue);
 
             // Целое со знаком. Диапазон - от -2147483648 до +2147483647
-            int k = -2147483648;
-            Int32 l = +2147483647;
-            Console.WriteLine("Type int:    default value {0}, min {1}, max {2}", 0, k, l);
+            int myInt = new int();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myInt.GetType(), myInt, int.MinValue, int.MaxValue);
 
             // 64-bits == 8 bytes
             // Целое без знака. Диапазон - от 0 до 18446744073709551615
-            ulong m = 0;
-            UInt64 n = 18446744073709551615;
-            Console.WriteLine("Type ulong:  default value {0}, min {1}, max {2}", 0, m, n);
+            ulong myUlong = new ulong();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myUlong.GetType(), myUlong, ulong.MinValue, ulong.MaxValue);
 
             // Целое со знаком. Диапазон - от -9223372036854775808 до +9223372036854775807
-            long o = -9223372036854775808;
-            Int64 p = +9223372036854775807;
-            Console.WriteLine("Type long:   default value {0}, min {1}, max {2}", 0L, o, p);
+            long myLong = new long();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myLong.GetType(), myLong, long.MinValue, long.MaxValue);
             #endregion
 
             #region Вещественные типы
             // Вещественные типы предназначены для представления дробных чисел
             // 32-bits == 4 bytes
             // Вещественное со знаком. Диапазон - от +/-1.5 x 10^-45 до +/-3.4 x 10^38
-            float q = -0.123456789f;
-            Single r = +1.123456789F;
-            Console.WriteLine("Type float:    default value {0}, min {1}, max {2}", 0.0F, q, r);
+            float myFloat = new float();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myFloat.GetType(), myFloat, float.MinValue, float.MaxValue);
 
             // 64-bits == 8 bytes
             // Вещественное со знаком. Диапазон - от +/-5.0 x 10^-324 до +/-1.7 x 10^308
-            double s = -0.123456789d;
-            Double t = +1.123456789;
-            Console.WriteLine("Type double:   default value {0}, min {1}, max {2}", 0.0D, s, t);
+            double myDouble = new double();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myDouble.GetType(), myDouble, double.MinValue, double.MaxValue);
             #endregion
 
             #region Десятичный тип
             // Десятичное со знаком. Диапазон - от от +/-1.0 x 10^-28 до +/-7.9 x 10^28
-            decimal u = -0.12345m;
-            Decimal v = +1.1234567M;
-            Console.WriteLine("Type decimal:  default value {0}, min {1}, max {2}", 0.0M, u, v);
+            decimal myDecimal = new decimal();
+            Console.WriteLine("Type {0}: default value {1}, min {2}, max {3}", myDecimal.GetType(), myDecimal, decimal.MinValue, decimal.MaxValue);
             #endregion
 
             #region Логический тип
             // 8-bits == 1 byte
             // Логическая величина Истина/Ложь
-            bool w = true;
-            Boolean x = false;
-            Console.WriteLine("Type bool:     default value {0}", false);
+            bool myBool = new bool();
+            Console.WriteLine("Type {0}: default value {1}", myBool.GetType(), myBool);
             #endregion
 
             #region Символьный тип
             // 16-bits == 2 bytes
             // Символ в формате UNICODE
-            char y = 'a';
-            Char z = 'A';
-            Console.WriteLine("Type char:     default value {0}", '\0');
+            char myСhar = new char();
+            Console.WriteLine("Type {0}: default value {1}", myСhar.GetType(), myСhar);
             #endregion
+
+            
+
 
             Console.ReadKey();
         }
