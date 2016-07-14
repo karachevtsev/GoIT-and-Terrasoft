aafbf5bd037365aeaa9e92c2f3ab9beecfd3196c
karachevtsev <karachevtsev@yahoo.com>
2016-07-15 02:27:14 +0300

    Fixed Homework Task 1.2 with cycle

--- Lesson 1. Types and Variables/Task 1.2/Task 1.2/Program.cs
+++ Lesson 1. Types and Variables/Task 1.2/Task 1.2/Program.cs
@@ -10,61 +10,14 @@ namespace Task_1._2
     {
         static void Main(string[] args)
         {
-            char h = 'H';
-            char e = 'e';
-            char l = 'l';
-            char o = 'o';
-            char w = 'w';
-            char r = 'r';
-            char d = 'd';
+           string str = "Hello world";
 
-            int valueH = Convert.ToInt32(h);
-            int valueE = Convert.ToInt32(e);
-            int valueL = Convert.ToInt32(l);
-            int valueO = Convert.ToInt32(o);
-            int valueW = Convert.ToInt32(w);
-            int valueR = Convert.ToInt32(r);
-            int valueD = Convert.ToInt32(d);
-
-            string hexOutputH = String.Format("{0:X}", valueH);
-            string hexOutputE = String.Format("{0:X}", valueE);
-            string hexOutputL = String.Format("{0:X}", valueL);
-            string hexOutputO = String.Format("{0:X}", valueO);
-            string hexOutputW = String.Format("{0:X}", valueW);
-            string hexOutputR = String.Format("{0:X}", valueR);
-            string hexOutputD = String.Format("{0:X}", valueD);
-
-            decimal convertDecimalH = Convert.ToDecimal(valueH);
-            decimal convertDecimalE = Convert.ToDecimal(valueE);
-            decimal convertDecimalL = Convert.ToDecimal(valueL);
-            decimal convertDecimalO = Convert.ToDecimal(valueO);
-            decimal convertDecimalW = Convert.ToDecimal(valueW);
-            decimal convertDecimalR = Convert.ToDecimal(valueR);
-            decimal convertDecimalD = Convert.ToDecimal(valueD);
-
-            Console.WriteLine("Hexadecimal value of {0} is {1}", h, hexOutputH);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", e, hexOutputE);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", l, hexOutputL);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", l, hexOutputL);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", o, hexOutputO);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", w, hexOutputW);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", o, hexOutputO);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", r, hexOutputR);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", l, hexOutputL);
-            Console.WriteLine("Hexadecimal value of {0} is {1}", d, hexOutputD);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Decimal value of {0} is {1}", h, convertDecimalH);
-            Console.WriteLine("Decimal value of {0} is {1}", e, convertDecimalE);
-            Console.WriteLine("Decimal value of {0} is {1}", l, convertDecimalL);
-            Console.WriteLine("Decimal value of {0} is {1}", l, convertDecimalL);
-            Console.WriteLine("Decimal value of {0} is {1}", o, convertDecimalO);
-            Console.WriteLine("Decimal value of {0} is {1}", w, convertDecimalW);
-            Console.WriteLine("Decimal value of {0} is {1}", o, convertDecimalO);
-            Console.WriteLine("Decimal value of {0} is {1}", r, convertDecimalR);
-            Console.WriteLine("Decimal value of {0} is {1}", l, convertDecimalL);
-            Console.WriteLine("Decimal value of {0} is {1}", d, convertDecimalD);
-
+            for (int i = 0; i < str.Length; i++)
+            {
+                int value = Convert.ToInt32(str[i]);
+                string hexOutput = String.Format("{0:X}", value);
+                Console.WriteLine("Char {0,2}: dec{1,5} hex {2,4}", str[i], value, hexOutput);
+            }
 
             Console.ReadKey();
         }
