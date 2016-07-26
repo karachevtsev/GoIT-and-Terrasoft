d0ac70195264e49d9e21e0ae4c091eedf94b2618
karachevtsev <karachevtsev@yahoo.com>
2016-07-27 00:38:07 +0300

    Fixed task 1.1, add solution for mentor task 2.1

--- Lesson 1. Types and Variables/Task-1.1/Task-1.1/Program.cs
+++ Lesson 1. Types and Variables/Task-1.1/Task-1.1/Program.cs
@@ -10,37 +10,38 @@ namespace Task_1._1
     {
         static void Main(string[] args)
         {
-            #region Целые типы
-            Console.WriteLine("Type byte: default value {0}, min {1}, max {2}", default(byte), byte.MinValue, byte.MaxValue);
-            Console.WriteLine("Type sbyte: default value {0}, min {1}, max {2}", default(sbyte), sbyte.MinValue, sbyte.MaxValue);
-            Console.WriteLine("Type short: default value {0}, min {1}, max {2}", default(short), short.MinValue, short.MaxValue);
-            Console.WriteLine("Type ushort: default value {0}, min {1}, max {2}", default(ushort), ushort.MinValue, ushort.MaxValue);
-            Console.WriteLine("Type int: default value {0}, min {1}, max {2}", default(int), int.MinValue, int.MaxValue);
-            Console.WriteLine("Type uint: default value {0}, min {1}, max {2}", default(uint), uint.MinValue, uint.MaxValue);
-            Console.WriteLine("Type long: default value {0}, min {1}, max {2}", default(long), long.MinValue, long.MaxValue);
-            Console.WriteLine("Type ulong: default value {0}, min {1}, max {2}", default(ulong), ulong.MinValue, ulong.MaxValue);
-            #endregion
-
-            #region Вещественные типы
-            Console.WriteLine("Type float: default value {0}, min {1}, max {2}", default(float), float.MinValue, float.MaxValue);
-            Console.WriteLine("Type double: default value {0}, min {1}, max {2}", default(double), double.MinValue, double.MaxValue);
-            #endregion
-
-            #region Десятичный тип
-            Console.WriteLine("Type decimal: default value {0}, min {1}, max {2}", default(decimal), decimal.MinValue, decimal.MaxValue);
-            #endregion
-
-            #region Логический тип
-            Console.WriteLine("Type bool: default value {0}", default(bool));
-            #endregion
-
-            #region Символьный тип
-            Console.WriteLine("Type char: default value {0}", default(char));
-            #endregion
-
+            List<object> types = new List<object>()
+            {
+                default(byte), byte.MinValue, byte.MaxValue,
+                default(sbyte), sbyte.MinValue, sbyte.MaxValue,
+                default(short), short.MinValue, short.MaxValue,
+                default(ushort), ushort.MinValue, ushort.MaxValue,
+                default(int), int.MinValue, int.MaxValue,
+                default(uint), uint.MinValue, uint.MaxValue,
+                default(long), long.MinValue, long.MaxValue,
+                default(ulong), ulong.MinValue, ulong.MaxValue,
+                default(float), float.MinValue, float.MaxValue,
+                default(double), double.MinValue, double.MaxValue,
+                default(decimal), decimal.MinValue, decimal.MaxValue,
+                default(bool),"____","____",
+                default(char),"____","____"
+            };
+            Types.ShowTypeList(types);
             Console.ReadKey();
         }
 
+        public class Types
+        {
+            public static void ShowTypeList(List<object> typesList)
+            {
+                for (int i = 0; i < typesList.Count; i = i + 3)
+                {
+                    Console.WriteLine("Type " + typesList[i].GetType() + ": default value " + typesList[i] + ", min " + typesList[i + 1] + ", max " + typesList[i + 2]);
+                }
+            }
+        }
+
+       
 
     }
 }
--- Lesson 2. Operators/Task 2.1/Task 2.1/Program.cs
+++ Lesson 2. Operators/Task 2.1/Task 2.1/Program.cs
@@ -10,6 +10,29 @@ namespace Task_2._1
     {
         static void Main(string[] args)
         {
+            int a;
+            int b;
+            string res = "";
+
+            a = 1234567890;
+
+            while (a > 0)
+            {
+                b = a % 10; 
+                if (b % 2 != 0 && b != 9)
+                {
+                    b++;
+                }
+                else if (b == 9)
+                {
+                    b = 0;
+                }
+                string str = b.ToString();
+                res = str + res;
+                a /= 10;
+            }
+            Console.WriteLine(res);
+            Console.ReadKey();
         }
     }
 }
